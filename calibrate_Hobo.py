c5dc530127d6d0d192f1e13a14e67604ca086bbb
gregsinnett <gsinnett@gmail.com>
2023-05-03 18:48:50 -0700

    start code to calibrate using means

--- calibrate_Hobo.py
+++ calibrate_Hobo.py
@@ -13,7 +13,7 @@ datasets = xr.open_mfdataset(file_pattern, concat_dim="SN", combine="nested")
 datasets = datasets.sortby('Time')
 
 # plot all the data
-[plt.scatter(datasets.Time,datasets.temp_C[SN]) for SN in datasets.SN]
+# [plt.scatter(datasets.Time,datasets.temp_C[SN]) for SN in datasets.SN]
 
 # Clip the dataset to isolate the calibration period
 # Define the time range to clip to
@@ -24,6 +24,14 @@ end_time = '2023-04-26T16:33'
 ds_clipped = datasets.sel(Time=slice(start_time, end_time)).sel(Time=slice(start_time, end_time))
 
 # plot the clipped data
-[plt.scatter(ds_clipped.Time,ds_clipped.temp_C[SN]) for SN in datasets.SN]
+fig, ax = plt.subplots()
+[ax.scatter(ds_clipped.Time,ds_clipped.temp_C[SN]) for SN in datasets.SN]
+ax.set_ylabel('Temp C')
+ax.grid(True)
+
+
+# Find the mean at each time step **working here
+means = [ds_clipped.temp_C[:,SN].mean().values for SN in ds_clipped.SN]
+ds_clipped.temp_C.sel(SN = 0).mean().values
 
 print()
