82c05e0efedc9a03eb9de3a8258cd6ea2c58aaea
gregsinnett <gsinnett@gmail.com>
2023-05-16 23:08:33 -0700

    Add lat and lon and save a CSV file for arcGIS

--- CTD_analysis.py
+++ CTD_analysis.py
@@ -25,7 +25,12 @@ Tmp = []
 Den = []
 Dpth = []
 Press = []
+Lat = []
+Lon = []
 for filename in filenames:
+    dfh = pd.read_csv(filename, nrows=27) # Read the header (assuming 27 rows)
+    lat = dfh[dfh['% Device'].str.contains('Start latitude')].values[0,1]
+    lon = dfh[dfh['% Device'].str.contains('Start longitude')].values[0,1]
     df = pd.read_csv(filename, header=28)
     mask = df.columns.str.contains('Temp.*')
     Tdata  = df.loc[:,mask] # selects mask
@@ -35,17 +40,27 @@ for filename in filenames:
     Dpthdata  = df.loc[:,mask] # selects mask
     mask = df.columns.str.contains('Pressure.*')
     Pressdata  = df.loc[:,mask] # selects mask
+    latdata = pd.DataFrame({'Latitude': [lat]*Tdata.size})
+    londata = pd.DataFrame({'Longitude': [lon]*Tdata.size})
+    Lat.append(latdata)
+    Lon.append(londata)
     Tmp.append(Tdata)
     Den.append(Ddata)
     Dpth.append(Dpthdata)
     Press.append(Pressdata)
 
+Lat = pd.concat(Lat,ignore_index=True)
+Lon = pd.concat(Lon,ignore_index=True)
 Tmp = pd.concat(Tmp,ignore_index=True)
 Den = pd.concat(Den,ignore_index=True)
 Dpth = pd.concat(Dpth,ignore_index=True)
 Press = pd.concat(Press,ignore_index=True)
 
 PotTemp = gsw.pt0_from_t(0,Tmp,Press) # Option to calculate potential temperature for deep casts
+
+df_full = pd.concat([Lat, Lon, Tmp, Den, Dpth, Press], axis=1)
+# Save the DataFrame as a CSV file
+df_full.to_csv('Altaussee_CTD.csv', index=False)
 
 #%% Plot Temperature vs. Density with Depth
 ax = plt.subplot()
@@ -76,3 +91,4 @@ plt.ylabel('Depth (m)')
 plt.xticks(range(4, 21, 2)) # Change the font size here
 plt.show()
 # %%
+print()
