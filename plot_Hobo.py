0172e75d3daae61bbc076eb862dff73e8a589ccf
gregsinnett <gsinnett@gmail.com>
2023-06-21 23:04:07 +0200

    modify plotting script

--- plot_Hobo.py
+++ plot_Hobo.py
@@ -3,6 +3,8 @@ import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
+import matplotlib.dates as mdates
+
 
 
 # basefile = '21721686_cal'
@@ -29,16 +31,28 @@ end_time = '2023-06-17T10:00'
 # Clip the dataset to the specified time range
 ds_clipped = datasets.sel(Time=slice(start_time, end_time)).sel(Time=slice(start_time, end_time))
 
+Depths = ['0.2m', '1m', '2m', '3m','5m', '7m','9m','11m', '13m','20m','35m','45m','65m']
 # plot all the data
 fig, ax = plt.subplots()
 for inst in datasets.Num:
-    ax.scatter(ds_clipped.Time,ds_clipped.temp_C[inst],4)
-    ax.set_ylabel('Temp C')
+    ax.scatter(ds_clipped.Time,ds_clipped.temp_C[inst],5,label = Depths[inst.values])
+    ax.set_ylabel('Temp [C]')
     ax.grid(True)
+# Set the date formatter
+date_formatter = mdates.DateFormatter('%b %d %H:%M')
+ax.xaxis.set_major_formatter(date_formatter)
+# Adjust x-axis tick label rotation and alignment
+plt.xticks(rotation=45, ha='right')
+# Adjust the position of the plot within the figure
+plt.subplots_adjust(left=0.15, right=0.8, bottom=0.2, top=0.9)  # Adjust the values as desired
+plt.legend()
+# Add legend outside the plot on the right
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), title = 'Inst Depths')
+
 plt.show()
 
 # plot a requested sensor
-sensors = [3, 4, 5] #specify the desired sensor from the top (zero indexed)
+sensors = [3,4,5] #specify the desired sensor from the top (zero indexed)
 fig, ax = plt.subplots()
 for num in sensors:
     ax.scatter(ds_clipped.Time,ds_clipped.temp_C[num],4)
