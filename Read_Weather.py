a881e4ea3d6e6c59dd2e43e3d4eae145252528ae
gregsinnett <gsinnett@gmail.com>
2023-07-21 15:08:48 -0400

    modify to concat the first 3 days, plot and save .nc file

--- Read_Weather.py
+++ Read_Weather.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import xarray as xr
+import datetime
 import matplotlib.pyplot as plt
 
 import os
 from os import listdir
+
+run_checks = 'yes'
+save_file = 'yes'
 
 def find_csv_filenames( path_to_dir, suffix=".csv" ):
     filenames = listdir(path_to_dir)
@@ -13,17 +17,17 @@ def find_csv_filenames( path_to_dir, suffix=".csv" ):
 path = '/Users/gregsinnett/Google Drive/Shared drives/Projects/Altaussee/Data/Weather Station/'
 os.chdir(path) #change to the desired working directory
 filenames = find_csv_filenames('./') #find the .csv files in the current folder
+filenames.sort()
 
-for basefile in filenames:
+df_full = pd.DataFrame(columns=['Air Temp [C]', 'Bottom Water Temp [C]', 'Surface Water Temp [C]',
+       'Reserve', 'Wind Speed [m/s]', 'Max Wind Speed [m/s]',
+       'Wind Direction [deg]', 'Max Wind Direction [deg]', 'Air Temp [C]',
+       'Relative Hum [pct rh]', 'Absolute Hum [g/m^3]', 'Dew Point Temp [C]',
+       'Absolute air pressure [hPa]', 'Relative air pressure [hPa]',
+       'Radiation [W/m^2]', 'Precip Indicator [0/1]', 'Precip [mm/h]',
+       'Daily Precip [mm/day]', 'Precip Type'])
+for basefile in filenames[-3:]:
     file = basefile
-    outfile = basefile + '.nc'
-    run_checks = 'yes'
-    save_file = 'yes'
-
-
-    Tme = []
-    Tmp = []
-    W_det = []
 
     df = pd.read_csv(path + file, thousands='.', decimal=',', sep=';', header=0, index_col=False)
     # Manipulate the date to create a standard index of datetime
@@ -54,26 +58,30 @@ for basefile in filenames:
     df.rename(columns={'31405 Niederschlagsmenge_mm/d': 'Daily Precip [mm/day]'}, inplace=True)
     df.rename(columns={'31407 Niederschlagsart': 'Precip Type'}, inplace=True)
     
-    ds = xr.Dataset(df)
-    ds['SN'] = str(basefile[0:8]) # save the serial number
+    df_full = pd.concat([df_full, df], axis = 0)
 
-    # Calculate the mean sample rate
-    times = pd.to_datetime(ds['Time'].values)
-    dt = pd.Series(times).diff().mean()
-    sample_rate = dt.total_seconds()
+ds = xr.Dataset(df_full)
+ds = ds.rename({'dim_0':'time'})
 
+if run_checks == 'yes':
+    fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex=True)
+    ax1.scatter(ds.time,ds['Air Temp [C]'],color = 'black')
+    # Create a second y axis
+    axb = ax1.twinx()
+    axb.scatter(ds.time,ds['Surface Water Temp [C]'],color = 'red')
+    
+    ax1.grid(True)
+    ax1.set_ylabel('Air Temp [C]')
+    axb.set_ylabel('Surface Water Temp [C]', color = 'red')
 
-    if run_checks == 'yes':
-        fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex=True)
-        ax1.scatter(ds.Time,ds.Time)
-        ax1.grid(True)
-        ax1.set_title(f'Sample rate = {sample_rate:.2f} s')
-
-        ax2.scatter(ds.Time,ds.temp_C)
-        ax2.grid(True)
-        ax2.set_ylabel('Temp C')
-        plt.show()
+    ax2.scatter(ds.time,ds['Radiation [W/m^2]'])
+    ax2.grid(True)
+    ax2.set_ylabel('Radiation [W/m^2]')
+    plt.show()
 
     # Save the dataframe
-    if save_file == 'yes':
-        ds.to_netcdf(path + outfile)
+if save_file == 'yes':
+    # current_date = datetime.datetime.now()
+    # formatted_date = current_date.strftime("%Y_%m_%d")
+    outfile = 'AltausseeWeather' + '.nc'
+    ds.to_netcdf(path + outfile)
